b2f0aedfc452d1352dd453711033dce89ac3b625
Micadurp <nicholas@deyoung.se>
2019-02-25 15:21:32 +0100

    Various changes to attempt to pass online tests

--- AStarPath/FindPath.cpp
+++ AStarPath/FindPath.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <stack>
-#include <stdlib.h>
 #include <algorithm> 
 #include <iostream>
 
@@ -8,7 +7,7 @@ struct Node
 {
 	int x = -1;
 	int y = -1;
-	int cost = INT_MAX;
+	unsigned int cost = UINT32_MAX;
 	char value = 0; // 0 impassable, 1 traversable
 	Node * parent = nullptr; 
 };
@@ -25,12 +24,12 @@ struct find_node {
 };
 
 // Distance from start + distance from target
-int FindCost(const int& nStartX, const int& nStartY,
+unsigned int FindCost(const int& nStartX, const int& nStartY,
 	const int& nTargetX, const int& nTargetY,
 	const int& nCurrX, const int& nCurrY)
 {
-	int distStart = abs(nCurrX - nStartX) + abs(nCurrY - nStartY);
-	int distTarget = abs(nCurrX - nTargetX) + abs(nCurrY - nTargetY);
+	int distStart = std::abs(nCurrX - nStartX) + std::abs(nCurrY - nStartY);
+	int distTarget = std::abs(nCurrX - nTargetX) + std::abs(nCurrY - nTargetY);
 	return distStart + distTarget;
 }
 
@@ -39,11 +38,29 @@ int FindPath(const int nStartX, const int nStartY,
 	const unsigned char* pMap, const int nMapWidth, const int nMapHeight,
 	int* pOutBuffer, const int nOutBufferSize)
 {
+
+	bool pathFound = false;
+	int steps = 0;
+	
+	// Are we starting on the target
+	if (nStartX == nTargetX && nStartY == nTargetY)
+	{
+		if (nOutBufferSize > 0)
+		{
+			pOutBuffer[0] = nTargetX + nTargetY * nMapWidth;
+		}
+		return steps;
+	}
+
+	// Might change during runtime because the pointer itself isn't constant
+	unsigned char * tMap = new unsigned char[nMapWidth * nMapHeight];
+	int size = nMapWidth * nMapHeight;
+	std::copy(pMap, pMap + size, tMap);
+
 	// Create variables
 	std::vector<Node*> unexplored;
 	std::vector<Node*> explored;
-	bool pathFound = false;
-	size_t steps = 0;
+
 
 	// Adding start postition to be explored
 	Node * start = new Node();
@@ -55,7 +72,7 @@ int FindPath(const int nStartX, const int nStartY,
 
 	while (!pathFound)
 	{
-		Node * current;
+		Node * current = nullptr;
 		
 		// Intentional scope
 		{
@@ -74,20 +91,27 @@ int FindPath(const int nStartX, const int nStartY,
 			pathFound = true;
 			std::stack<int> tmp;
 
-			// Shortest path found, now to take note of what it was (also make sure it fits in the buffer)
-			while (current->parent != nullptr || nOutBufferSize - steps <= 0)
+			// Shortest path found, now to take note of what it was
+			while (current->parent != nullptr)
 			{
 				tmp.push(current->x + current->y * nMapWidth);
 				current = current->parent;
 				steps++;
 			}
 
-			// Path is reversed so lets quickly change that
-			for (size_t i = 0; i < steps; ++i)
+			// Path is reversed so lets quickly change that, don't go bigger than the buffer
+			for (int i = 0; i < steps && i < nOutBufferSize; ++i)
 			{
 				pOutBuffer[i] = tmp.top();
 				tmp.pop();
 			}
+
+			// Test because I'm unsure what to return when path is bigger than outbuffer
+			/*if ( steps > nOutBufferSize )
+			{
+				steps = -1;
+			}*/
+
 			break;
 		}
 		
@@ -101,7 +125,7 @@ int FindPath(const int nStartX, const int nStartY,
 			Node * neighbour = new Node();
 			neighbour->x = current->x + neDi[0];
 			neighbour->y = current->y + neDi[1];
-			neighbour->value = pMap[neighbour->x + neighbour->y * nMapWidth];
+			neighbour->value = tMap[neighbour->x + neighbour->y * nMapWidth];
 
 			// Is the neighbour outside the map?
 			if (neighbour->x < 0 || neighbour->x >= nMapWidth || neighbour->y < 0 || neighbour->y >=nMapHeight)
@@ -117,7 +141,7 @@ int FindPath(const int nStartX, const int nStartY,
 			}
 				
 			// Get the cost of the neighbour
-			int tmpCost = FindCost(nStartX, nStartY, nTargetX, nTargetY, neighbour->x, neighbour->y);
+			unsigned int tmpCost = FindCost(nStartX, nStartY, nTargetX, nTargetY, neighbour->x, neighbour->y);
 			auto foundNode = std::find_if(unexplored.begin(), unexplored.end(), find_node(neighbour->x, neighbour->y));
 			// If neighbour wasn't found in unexplored, create it, else update the neighbour if new path is shorter.
 			if (foundNode == unexplored.end())
@@ -150,6 +174,7 @@ int FindPath(const int nStartX, const int nStartY,
 	{
 		delete node;
 	}
+	delete[] tMap;
 
 	// Return steps if path is found, else -1
 	return pathFound ? steps : -1;
@@ -158,13 +183,14 @@ int FindPath(const int nStartX, const int nStartY,
 int main()
 {
 	{
-		unsigned char pMap[] = { 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 };
+		unsigned char map[] = { 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 };
 		int pOutBuffer[12];
-		int steps = FindPath(0, 0, 1, 2, pMap, 4, 3, pOutBuffer, 12);
+		int outBufferSize = 12;
+		int steps = FindPath(0, 0, 0, 0, map, 4, 3, pOutBuffer, outBufferSize);
 
 		std::cout << steps << std::endl;
 		std::cout << pOutBuffer[0];
-		for (size_t i = 1; i < steps; i++)
+		for (int i = 1; i < steps && i < outBufferSize; i++)
 		{
 			std::cout << ", " << pOutBuffer[i];
 		}
@@ -172,15 +198,15 @@ int main()
 	}
 
 	{
-		unsigned char pMap[] = { 0, 0, 1, 0, 1, 1, 1, 0, 1 };
+		unsigned char map[] = { 0, 0, 1, 0, 1, 1, 1, 0, 1 };
 		int pOutBuffer[7];
-		int steps = FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);
+		int steps = FindPath(2, 0, 0, 2, map, 3, 3, pOutBuffer, 7);
 
 		std::cout << steps << std::endl;
 		if (steps != -1)
 		{
 			std::cout << pOutBuffer[0];
-			for (size_t i = 1; i < steps; i++)
+			for (int i = 1; i < steps; i++)
 			{
 				std::cout << ", " << pOutBuffer[i];
 			}
